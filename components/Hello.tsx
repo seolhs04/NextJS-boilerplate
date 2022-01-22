bdbd5365786d858082d6d4f27421d2b680ed8e0b
seol <shs00421@gmail.com>
2022-01-22 17:33:50 +0900

    emotionJS 설치

--- components/Hello.tsx
+++ components/Hello.tsx
@@ -1,5 +1,12 @@
+import { css, Global, keyframes } from "@emotion/react";
+import styled from "@emotion/styled";
+
 export const sum = (a: number, b: number): number => a + b;
 
 export default function hello() {
-  return <div>hello</div>;
+  const CustomDiv = styled.div`
+    background: tomato;
+  `;
+
+  return <CustomDiv>hello</CustomDiv>;
 }
