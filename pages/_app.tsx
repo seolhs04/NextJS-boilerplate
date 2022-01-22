bc81e5d6e0bcec677126936ed3f1001ec9c581d2
seol <shs00421@gmail.com>
2022-01-22 16:07:53 +0900

    jest babel 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+// import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
+export const sum = (a: number, b: number): number => a + b;
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/app.test.ts
@@ -0,0 +1,5 @@
+import { sum } from "./_app";
+
+test("jest 테스트", () => {
+  expect(sum(1, 2)).toBe(3);
+});
